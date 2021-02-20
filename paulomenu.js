a16177f7b3cc71b06ae37cf63e2fdae74ad8849f
jaozin7 <79337355+jaozin7@users.noreply.github.com>
2021-02-19 22:37:19 -0300

    Jaozin
    Jaozin

---
+++ paulomenu.js
@@ -0,0 +1,12 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *Comandos do 𝐁𝐎𝐓 𝐃𝐎 𝐉𝐀𝐎𝐙𝐈𝐍*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = paulomenu
